cc8ba910a2be02f9c11e4c3edca66f76dfb73f0a
Dulaj Atapattu <dulaj.r.atapattu@gmail.com>
2017-06-14 22:25:53 +0530

    Adding learning capabilities

--- detector.py
+++ detector.py
@@ -2,7 +2,7 @@ import sys
 
 statSinhala = {}
 statTamil = {}
-statTarged = {}
+statTarget = {}
 
 meanAbsoluteError = {'sinhala': 0, 'tamil': 0}
 
@@ -11,7 +11,7 @@ detectedLanguage = ''
 cvcvvCountStatsSinhala = {'cv': {}, 'cvv': {}, 'letters': {}}
 cvcvvCountStatsTamil = {'cv': {}, 'cvv': {}, 'letters': {}}
 cvcvvCountStatsTarget = {'cv': {}, 'cvv': {}, 'letters': {}}
-
+error = False
 def calculateDistributions(sinhala, tamil, target):
     global cvcvvCountStatsSinhala, cvcvvCountStatsTamil, cvcvvCountStatsTarget
 
@@ -22,6 +22,12 @@ def calculateDistributions(sinhala, tamil, target):
         cvCount = eval(data[2].split('-')[1].strip())
         cvvCount = eval(data[3].split('-')[1].strip())
         cvcvvCount = eval(data[4].split('-')[1].strip())
+
+        statSinhala['totalLetters']  = totalLetters
+        statSinhala['letterCounts']  = letterCounts
+        statSinhala['cvCount']  = cvCount
+        statSinhala['cvvCount']  = cvvCount
+        statSinhala['cvcvvCount']  = cvcvvCount
 
         # Updating sinhala stats
         for key, value in cvcvvCount['cv'].iteritems():
@@ -41,6 +47,12 @@ def calculateDistributions(sinhala, tamil, target):
         cvvCount = eval(data[3].split('-')[1].strip())
         cvcvvCount = eval(data[4].split('-')[1].strip())
 
+        statTamil['totalLetters']  = totalLetters
+        statTamil['letterCounts']  = letterCounts
+        statTamil['cvCount']  = cvCount
+        statTamil['cvvCount']  = cvvCount
+        statTamil['cvcvvCount']  = cvcvvCount
+
         # Updating tamil stats
         for key, value in cvcvvCount['cv'].iteritems():
             cvcvvCountStatsTamil['cv'][key] = float(value)/cvCount
@@ -58,6 +70,12 @@ def calculateDistributions(sinhala, tamil, target):
         cvCount = eval(data[2].split('-')[1].strip())
         cvvCount = eval(data[3].split('-')[1].strip())
         cvcvvCount = eval(data[4].split('-')[1].strip())
+
+        statTarget['totalLetters']  = totalLetters
+        statTarget['letterCounts']  = letterCounts
+        statTarget['cvCount']  = cvCount
+        statTarget['cvvCount']  = cvvCount
+        statTarget['cvcvvCount']  = cvcvvCount
 
         # Updating target stats
         for key, value in cvcvvCount['cv'].iteritems():
@@ -87,8 +105,13 @@ def calculateError():
 
 initialParams = sys.argv
 
+targetStats = ''
+sinhalaStats = ''
+tamilStats = ''
+
 if len(initialParams) != 4:
     print "Use python detector.py <TARGET STATS> <SINHALA STATS> <TAMIL STATS>"
+    error = True
 else:
     targetStats = initialParams[1]
     sinhalaStats = initialParams[2]
@@ -100,9 +123,62 @@ else:
     if meanAbsoluteError['sinhala'] < meanAbsoluteError['tamil']:
         print 'Language detected: SINHALA'
         print 'MAE: ' + str(meanAbsoluteError['sinhala']) + ' < ' + str(meanAbsoluteError['tamil'])
+        detectedLanguage = 'SINHALA'
     elif meanAbsoluteError['sinhala'] > meanAbsoluteError['tamil']:
         print 'Language detected: TAMIL'
         print 'MAE: ' + str(meanAbsoluteError['tamil']) + ' < ' + str(meanAbsoluteError['sinhala'])
+        detectedLanguage = 'TAMIL'
     else:
+        error = True
         print 'Language detection failed, please provide a larger data set for training/testing'
 
+if not error:
+    print 'Was the prediction true and you need to update the stats? (Y/N) [N]'
+    if raw_input() == 'Y':
+        if detectedLanguage == 'SINHALA':
+            # Updating sinhala stats
+            statSinhala['totalLetters']  += statTarget['totalLetters']
+
+            for key, value in statTarget['letterCounts'].iteritems():
+                statSinhala['letterCounts'][key] += value
+
+            statSinhala['cvCount']  += statTarget['cvCount']
+            statSinhala['cvvCount']  += statTarget['cvvCount']
+            
+            for key, value in statTarget['cvcvvCount']['cv'].iteritems():
+                statSinhala['cvcvvCount']['cv'][key] += value
+            
+            for key, value in statTarget['cvcvvCount']['cvv'].iteritems():
+                statSinhala['cvcvvCount']['cvv'][key] += value
+            
+            with open(sinhalaStats, 'wb') as f:
+                f.writelines("totalLetters- " + str(statSinhala['totalLetters']))
+                f.writelines("\nletterCounts- " + str(statSinhala['letterCounts']))
+                f.writelines("\ncvCount- " + str(statSinhala['cvCount']))
+                f.writelines("\ncvvCount- " + str(statSinhala['cvvCount']))
+                f.writelines("\ncvcvvCount- " + str(statSinhala['cvcvvCount']))
+
+        elif detectedLanguage == 'TAMIL':
+            # Updating tamil statis
+            statTamil['totalLetters']  += statTarget['totalLetters']
+
+            for key, value in statTarget['letterCounts'].iteritems():
+                statTamil['letterCounts'][key] += value
+
+            statTamil['cvCount']  += statTarget['cvCount']
+            statTamil['cvvCount']  += statTarget['cvvCount']
+            
+            for key, value in statTarget['cvcvvCount']['cv'].iteritems():
+                statTamil['cvcvvCount']['cv'][key] += value
+            
+            for key, value in statTarget['cvcvvCount']['cvv'].iteritems():
+                statTamil['cvcvvCount']['cvv'][key] += value
+            
+            with open(tamilStats, 'wb') as f:
+                f.writelines("totalLetters- " + str(statTamil['totalLetters']))
+                f.writelines("\nletterCounts- " + str(statTamil['letterCounts']))
+                f.writelines("\ncvCount- " + str(statTamil['cvCount']))
+                f.writelines("\ncvvCount- " + str(statTamil['cvvCount']))
+                f.writelines("\ncvcvvCount- " + str(statTamil['cvcvvCount']))
+        print 'Updated the language library'
+    
